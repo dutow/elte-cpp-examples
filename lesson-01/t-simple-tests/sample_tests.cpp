ca8bdc978e385bc314d0e8576610ea6d4a4473b3
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2016-02-06 02:36:20 -0800

    added sample tests for the first lesson

---
+++ lesson-01/t-simple-tests/sample_tests.cpp
@@ -0,0 +1,80 @@
+
+#include "gtest/gtest.h"
+
+int sum_dividible_by_seven(int min, int max)
+{
+  int sum = 0;
+  for(int i=min; i<=max; ++i)
+  {
+    if(i % 7 == 0)
+    {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+/**
+ * Visszaadja a parameterkent megadott szam gyokenel kisebb vagy egyenlo legnagyobb egesz szamot
+ */
+unsigned int ceil_sqrt(unsigned int number)
+{
+	switch(number)
+	{
+		case 0: return 0;
+		case 1:
+		case 2:
+		case 3:
+			return 1;
+		default:
+		{
+			return 2; // TODO
+		}
+	}
+}
+
+TEST(sum_dividible_by_seven, empty_interval_is_zero)
+{
+  EXPECT_EQ(0, sum_dividible_by_seven(0,0));
+}
+
+TEST(sum_dividible_by_seven, interval_without_anything_matching)
+{
+  EXPECT_EQ(0, sum_dividible_by_seven(0,6));
+}
+
+TEST(sum_dividible_by_seven, just_14)
+{
+  EXPECT_EQ(14, sum_dividible_by_seven(14,14));
+}
+
+TEST(sum_dividible_by_seven, just_14_21)
+{
+  EXPECT_EQ(35, sum_dividible_by_seven(14,21));
+}
+
+TEST(ceil_sqrt, zero_is_zero)
+{
+  EXPECT_EQ(0, ceil_sqrt(0));
+}
+
+TEST(ceil_sqrt, one_is_one)
+{
+  EXPECT_EQ(1, ceil_sqrt(1));
+}
+
+TEST(ceil_sqrt, with_16)
+{
+  EXPECT_EQ(4, ceil_sqrt(16));
+}
+
+TEST(ceil_sqrt, with_26)
+{
+  EXPECT_EQ(5, ceil_sqrt(26));
+}
+
+TEST(ceil_sqrt, with_35)
+{
+  EXPECT_EQ(5, ceil_sqrt(35));
+}
